8bf736a9c4612a12a89c8ec310deb4a5386232cf
Vahagn <vgpnh.hk@mail.ru>
2021-08-23 10:57:59 +0400

    Fixes

--- ui/src/components/navbar/LinkCard.js
+++ ui/src/components/navbar/LinkCard.js
@@ -5,7 +5,7 @@ export const LinkCard = ({ link }) => {
       <div className="container" style={{position: 'relative', margin: 'auto'}}>
         <h2>Link</h2>
 
-        <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
+        <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">Click to follow the link</a></p>
         <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
         <p>Number of clicks: <strong>{link.clicks}</strong></p>
         <p>Date of creation: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
