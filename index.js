25ece7523b2ffb41908f6e65f283a91fc24ac7cd
Vahagngn <Vahagngn@gmail.com>
2021-10-20 09:18:43 +0400

    bugFix Chat

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 5000
 const app = express().use('*', cors());
 const http = require('http').Server(app);
 
-// app.use(
-//   express.static(path.join(__dirname, "ui/build"))
-// );
+app.use(
+  express.static(path.join(__dirname, "ui/build"))
+);
 
-// app.get('/:path', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'ui/build', 'index.html'));
-// });
-
+app.get('/:path', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ui/build', 'index.html'));
+});
+// 
 // app.get(`/:path`, (req, res) => {
 //   res.sendFile(
 //       path.join(__dirname,  "/ui/build/index.html")
@@ -29,7 +29,7 @@ const http = require('http').Server(app);
 
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(todoRoutes)
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
